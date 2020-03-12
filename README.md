2b601015a0237b03d777598686dde54d96a2e2c9
vlad <ionov1vlad@gmail.com>
2020-03-12 18:02:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+
+## How to use
+
+First, install [Webpack](https://www.npmjs.com/package/webpack) and [webpack-dev-server](https://www.npmjs.com/package/webpack-dev-server) globally.
+
+```bash
+$ npm i -g webpack webpack-dev-server
+```
+
+Install the dependencies.
+
+```bash
+$ npm init
+$ npm install
+```
+
+Run the project.
+
+```bash
+$ npm run dev
+```
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+    ]
+  }
+};
